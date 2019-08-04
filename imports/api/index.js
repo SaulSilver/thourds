f1110f5c447bc504845efdbc332bce9bc50955dc
hatem <hatem.houssein@gmail.com>
2019-08-04 11:11:35 +0900

    Plan backend operations (WIP)

--- imports/api/index.js
+++ imports/api/index.js
@@ -1,3 +1,14 @@
 import { check } from 'meteor/check';
 
+/**
+ * DB
+ * 1. Initiate the mongodb
+ * 2. Add collection for words to contain 1000 japanese words
+ */
+
+/**
+ * API
+ * 1. REST API
+ * 2. Use Matsumoto tweets
+ */
 Meteor.methods({});
